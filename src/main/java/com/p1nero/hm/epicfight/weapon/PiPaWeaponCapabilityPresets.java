540c2c0358df43a73c583e8c5f27cec2d8a67ec7
P1nero <1070402601@qq.com>
2024-12-26 18:34:59 +0800

    mixin连段

--- src/main/java/com/p1nero/hm/epicfight/weapon/PiPaWeaponCapabilityPresets.java
+++ src/main/java/com/p1nero/hm/epicfight/weapon/PiPaWeaponCapabilityPresets.java
@@ -4,9 +4,13 @@ import com.p1nero.hm.EpicFightHM;
 import com.p1nero.hm.epicfight.animation.PiPaAnimations;
 import com.p1nero.hm.epicfight.skill.PiPaSkills;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import reascer.wom.gameasset.WOMAnimations;
+import yesman.epicfight.api.animation.LivingMotions;
+import yesman.epicfight.api.collider.MultiOBBCollider;
 import yesman.epicfight.api.forgeevent.WeaponCapabilityPresetRegistryEvent;
 import yesman.epicfight.gameasset.Animations;
 import yesman.epicfight.gameasset.EpicFightSkills;
@@ -20,6 +24,29 @@ import java.util.function.Function;
 @Mod.EventBusSubscriber(modid = EpicFightHM.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class PiPaWeaponCapabilityPresets {
 
+    public static final Function<Item, CapabilityItem.Builder> YING_HUA = (item) ->
+            (CapabilityItem.Builder) WeaponCapability.builder().category(CapabilityItem.WeaponCategories.SWORD)
+                    .styleProvider((playerpatch) -> playerpatch.getHoldingItemCapability(InteractionHand.OFF_HAND)
+                            .getWeaponCategory() == CapabilityItem.WeaponCategories.SWORD ? CapabilityItem.Styles.TWO_HAND : CapabilityItem.Styles.ONE_HAND)
+                    .collider(new MultiOBBCollider(3, 0.4, 0.4, 0.0, 0.8, 0.0, -0.75))
+                    .swingSound(EpicFightSounds.WHOOSH_BIG.get())
+                    .hitSound(EpicFightSounds.BLADE_RUSH_FINISHER.get())
+                    .hitParticle(EpicFightParticles.HIT_BLUNT.get())
+                    .canBePlacedOffhand(false)
+                    .newStyleCombo(
+                            CapabilityItem.Styles.TWO_HAND,
+                            WOMAnimations.ANTITHEUS_AUTO_1,
+                            WOMAnimations.ANTITHEUS_AUTO_2,
+                            WOMAnimations.ANTITHEUS_AUTO_3,
+                            WOMAnimations.ANTITHEUS_AUTO_4,
+                            WOMAnimations.TORMENT_CHARGED_ATTACK_1,
+                            WOMAnimations.ANTITHEUS_AGRESSION,
+                            WOMAnimations.ANTITHEUS_GUILLOTINE)
+                    .innateSkill(CapabilityItem.Styles.TWO_HAND, (itemstack) -> EpicFightSkills.SWEEPING_EDGE)
+                    .livingMotionModifier(CapabilityItem.Styles.TWO_HAND, LivingMotions.BLOCK, WOMAnimations.AGONY_GUARD)
+                    .livingMotionModifier(CapabilityItem.Styles.TWO_HAND, LivingMotions.IDLE, WOMAnimations.SOLAR_IDLE)
+                    .livingMotionModifier(CapabilityItem.Styles.TWO_HAND, LivingMotions.WALK, WOMAnimations.HERRSCHER_WALK)
+                    .comboCancel((style) -> false);
 
     public static final Function<Item, CapabilityItem.Builder> WSFH = (item) ->
             (CapabilityItem.Builder) WeaponCapability.builder().category(PiPaWeaponCategories.WSFH)
@@ -43,6 +70,7 @@ public class PiPaWeaponCapabilityPresets {
     @SubscribeEvent
     public static void register(WeaponCapabilityPresetRegistryEvent event) {
         event.getTypeEntry().put(new ResourceLocation(EpicFightHM.MOD_ID, "wsfh"), WSFH);
+        event.getTypeEntry().put(new ResourceLocation(EpicFightHM.MOD_ID, "ying_hua"), YING_HUA);
     }
 
 }
---
+++ src/main/java/com/p1nero/hm/mixin/WeaponCapabilityMixin.java
@@ -0,0 +1,16 @@
+package com.p1nero.hm.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import yesman.epicfight.world.capabilities.entitypatch.LivingEntityPatch;
+import yesman.epicfight.world.capabilities.item.WeaponCapability;
+
+@Mixin(value = WeaponCapability.class, remap = false)
+public class WeaponCapabilityMixin {
+    @Inject(method = "shouldCancelCombo", at = @At("HEAD"), cancellable = true)
+    private void inject(LivingEntityPatch<?> entitypatch, CallbackInfoReturnable<Boolean> cir){
+        cir.setReturnValue(false);
+    }
+}
